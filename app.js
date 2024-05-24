ae89c2d6f7a7144fa0402a79789c8b0aeb8f7c80
Oscar <oscar15-91@hotmail.com>
2024-05-24 10:52:02 +0200

    cambios en app

--- app.js
+++
@@ -1,9 +0,0 @@
-let harina = "2 tazas de harina";
-let levadura = "1 cucharada de levadura";
-let azucar = "1 cucharada de azucar";
-let sal="20 gramos de sal";
-let agua = "1 taza de agua";
-let aceite = "2 rociadas de aceite";
-let mantequilla = "1 cuchara de mantequilla";
-let salsamarinera = "1 taza de salsa marinara";
-let quesomozarella="6 onzas de queso";
